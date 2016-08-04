5690e13bc5912016a308640eddf5351babad7a88
Skullywag <mjskull@gmail.com>
2016-08-04 20:42:54 +0100

    rewrote detour

---
+++ Source/ReclaimFabric/SpecialThingFilterWorker_ReclaimArmor.cs
@@ -0,0 +1,21 @@
+﻿using Verse;
+
+namespace ReclaimFabric
+{
+    
+	public class SpecialThingFilterWorker_ReclaimArmor : SpecialThingFilterWorker
+	{
+
+        public override bool                Matches( Thing t )
+		{
+			return AlwaysMatches( t.def );
+		}
+
+		public override bool                AlwaysMatches( ThingDef def )
+		{
+            return def.IsArmor();
+		}
+
+	}
+
+}
--- Source/ReclaimFabric/_Thing.cs
+++ Source/ReclaimFabric/_Thing.cs
@@ -14,20 +14,35 @@ namespace ReclaimFabric
     {
         internal static IEnumerable<Thing> _SmeltProducts(this Thing _this, float efficiency)
         {
-            if (_this.def.smeltProducts != null)
+            var costListAdj = _this.CostListAdjusted();
+            if (!costListAdj.NullOrEmpty())
             {
-                foreach (var counter in _this.def.smeltProducts)
+                foreach (var thingCost in costListAdj)
                 {
-                    do
+                    if (!thingCost.thingDef.intricate)
                     {
-                        var thing = ThingMaker.MakeThing(counter.thingDef);
-                        thing.stackCount = Mathf.Min(thing.def.stackLimit, counter.count);
-                        counter.count -= thing.stackCount;
-                        yield return thing;
-                    } while (counter.count > 0);
+                        var twentyFivePercentLimit = (float)thingCost.count * 0.25f;
+                        var randomUpToTwentyFivePercent = GenMath.RoundRandom(twentyFivePercentLimit);
+                        if (randomUpToTwentyFivePercent > 0)
+                        {
+                            var resultantSmeltedThing = ThingMaker.MakeThing(thingCost.thingDef, null);
+                            resultantSmeltedThing.stackCount = randomUpToTwentyFivePercent;
+                            yield return resultantSmeltedThing;
+                        }
+                    }
                 }
             }
-            if (_this.def.costStuffCount >= 0)
+            if (!_this.def.smeltProducts.NullOrEmpty())
+            {
+                foreach (var smeltProduct in _this.def.smeltProducts)
+                {
+                    var resultantSmeltedThing = ThingMaker.MakeThing(smeltProduct.thingDef, null);
+                    resultantSmeltedThing.stackCount = smeltProduct.count;
+                    yield return resultantSmeltedThing;
+                }
+            }
+
+            if (_this.def.IsClothes())
             {
                 Pawn crafter = _this.Position.GetEdifice().InteractionCell.GetFirstPawn();
                 float skillPerc = ((float)crafter.skills.GetSkill(SkillDefOf.Crafting).level / 20);
@@ -36,6 +51,10 @@ namespace ReclaimFabric
                 float num1 = Mathf.Lerp(0f, 0.4f, healthPerc);
                 var mainSmeltProductCount = (_this.def.costStuffCount * num1 / _this.Stuff.VolumePerUnit) * num;
                 var adjustedCount = GenMath.RoundRandom(mainSmeltProductCount);
+                //float healthPerc = ((float)_this.HitPoints / (float)_this.MaxHitPoints);
+                //float num1 = Mathf.Lerp(0f, 0.4f, healthPerc);
+                //var mainSmeltProductCount = (_this.def.costStuffCount * num1 / _this.Stuff.VolumePerUnit) * efficiency;
+                //var adjustedCount = GenMath.RoundRandom(mainSmeltProductCount);
                 if (adjustedCount > 0)
                 {
                     do
