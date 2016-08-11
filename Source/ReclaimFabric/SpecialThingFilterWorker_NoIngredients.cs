712f44463bb5db8e8a960d5a680178105023dadf
Skullywag <mjskull@gmail.com>
2016-08-11 19:46:30 +0100

    rewrote detour

---
+++ Source/ReclaimFabric/SpecialThingFilterWorker_NoIngredients.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Verse;
+using RimWorld;
+
+namespace ReclaimFabric
+{
+    public class SpecialThingFilterWorker_NoIngredients : SpecialThingFilterWorker
+    {
+        public override bool Matches(Thing t)
+        {
+            return this.AlwaysMatches(t.def);
+        }
+
+        public override bool AlwaysMatches(ThingDef def)
+        {
+            return def.costList == null && def.stuffCategories == null;
+        }
+    }
+}
--- Source/ReclaimFabric/_Thing.cs
+++ Source/ReclaimFabric/_Thing.cs
@@ -15,7 +15,29 @@ namespace ReclaimFabric
         internal static IEnumerable<Thing> _SmeltProducts(this Thing _this, float efficiency)
         {
             var costListAdj = _this.CostListAdjusted();
-            if (!costListAdj.NullOrEmpty())
+            if (_this.def.IsClothes() || _this.def.IsAdvancedArmor() || _this.def.IsArmor())
+            {
+                Pawn crafter = _this.Position.GetEdifice().InteractionCell.GetFirstPawn();
+                float skillPerc = ((float)crafter.skills.GetSkill(SkillDefOf.Crafting).level / 20);
+                float num = Mathf.Lerp(0.5f, 1.5f, skillPerc);
+                float healthPerc = ((float)_this.HitPoints / (float)_this.MaxHitPoints);
+                float num1 = Mathf.Lerp(0f, 0.4f, healthPerc);
+                foreach (var thingCost in costListAdj)
+                {
+                    if (!thingCost.thingDef.intricate)
+                    {
+                        var PercentMaxLimit = (float)thingCost.count * num1;
+                        var mainSmeltProductCount = (PercentMaxLimit * num);
+                        if (mainSmeltProductCount > 0)
+                        {
+                            var resultantSmeltedThing = ThingMaker.MakeThing(thingCost.thingDef, null);
+                            resultantSmeltedThing.stackCount = (int)mainSmeltProductCount;
+                            yield return resultantSmeltedThing;
+                        }
+                    }
+                }
+            }
+            else if (!costListAdj.NullOrEmpty())
             {
                 foreach (var thingCost in costListAdj)
                 {
@@ -32,38 +54,13 @@ namespace ReclaimFabric
                     }
                 }
             }
-            if (!_this.def.smeltProducts.NullOrEmpty())
+            else if (!_this.def.smeltProducts.NullOrEmpty())
             {
                 foreach (var smeltProduct in _this.def.smeltProducts)
                 {
                     var resultantSmeltedThing = ThingMaker.MakeThing(smeltProduct.thingDef, null);
                     resultantSmeltedThing.stackCount = smeltProduct.count;
                     yield return resultantSmeltedThing;
-                }
-            }
-
-            if (_this.def.IsClothes())
-            {
-                Pawn crafter = _this.Position.GetEdifice().InteractionCell.GetFirstPawn();
-                float skillPerc = ((float)crafter.skills.GetSkill(SkillDefOf.Crafting).level / 20);
-                float num = Mathf.Lerp(0.5f, 1.5f, skillPerc);
-                float healthPerc = ((float)_this.HitPoints / (float)_this.MaxHitPoints);
-                float num1 = Mathf.Lerp(0f, 0.4f, healthPerc);
-                var mainSmeltProductCount = (_this.def.costStuffCount * num1 / _this.Stuff.VolumePerUnit) * num;
-                var adjustedCount = GenMath.RoundRandom(mainSmeltProductCount);
-                //float healthPerc = ((float)_this.HitPoints / (float)_this.MaxHitPoints);
-                //float num1 = Mathf.Lerp(0f, 0.4f, healthPerc);
-                //var mainSmeltProductCount = (_this.def.costStuffCount * num1 / _this.Stuff.VolumePerUnit) * efficiency;
-                //var adjustedCount = GenMath.RoundRandom(mainSmeltProductCount);
-                if (adjustedCount > 0)
-                {
-                    do
-                    {
-                        var thing = ThingMaker.MakeThing(_this.Stuff);
-                        thing.stackCount = Mathf.Min(thing.def.stackLimit, adjustedCount);
-                        adjustedCount -= thing.stackCount;
-                        yield return thing;
-                    } while (adjustedCount > 0);
                 }
             }
         }
