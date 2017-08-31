708cb2b4a52f629e90041897596e5d4734cd2e26
calvin-fisher <calvin.fisher@gmail.com>
2017-08-30 23:14:18 -0500

    Replaced SmeltProducts detour with truncated Harmony patch

--- Source/ReclaimFabric/Bootstrap.cs
+++ Source/ReclaimFabric/Bootstrap.cs
@@ -2,28 +2,17 @@
 using System.Reflection;
 using Verse;
 using RimWorld;
+using Harmony;
 
 namespace ReclaimFabric
 {
-    class Bootstrap : Def
+    [StaticConstructorOnStartup]
+    class Bootstrap
     {
-        private const BindingFlags UniversalBindingFlags = BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic;
-
         static Bootstrap()
         {
-            {
-                MethodInfo method3 = typeof(Thing).GetMethod("SmeltProducts", BindingFlags.Instance | BindingFlags.Public);
-                MethodInfo method4 = typeof(_Thing_ReclaimFabric).GetMethod("_SmeltProducts", BindingFlags.Static | BindingFlags.NonPublic);
-                Log.Message("Attempting detour from " + method3 + "to " + method4);
-                if (!Detours.TryDetourFromTo(method3, method4))
-                {
-                    Log.Error("Reclaim Fabric Detour failed");
-                    return;
-                }
-                Log.Message("Reclaim Fabric Detour Successful");
-            }
-
-            Assembly Assembly_CSharp = Assembly.Load("Assembly-CSharp.dll");
+            var harmony = HarmonyInstance.Create("com.github.pardeike.reclaimfabric");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
         }
     }
 }
--- Source/ReclaimFabric/_Thing_ReclaimFabric.cs
+++ Source/ReclaimFabric/_Thing_ReclaimFabric.cs
@@ -7,60 +7,48 @@ using System.Text.RegularExpressions;
 using UnityEngine;
 using Verse.AI;
 using Verse;
+using Harmony;
 
 namespace ReclaimFabric
 {
+    [HarmonyPatch(typeof(Thing))]
+    [HarmonyPatch("SmeltProducts")]
+    [HarmonyPatch(new Type[] { typeof(Thing), typeof(float) })]
     public static class _Thing_ReclaimFabric
     {
-        internal static IEnumerable<Thing> _SmeltProducts(this Thing _this, float efficiency)
+        static bool Prefix(Thing _instance, IEnumerable<Thing> _result, ref float efficiency)
+        {
+            if (_instance.def.IsClothes() || _instance.def.IsAdvancedArmor() || _instance.def.IsArmor())
+            {
+                _result = _SmeltProducts(_instance);
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        static IEnumerable<Thing> _SmeltProducts(Thing _this)
         {
             var costListAdj = _this.CostListAdjusted();
-            if (_this.def.IsClothes() || _this.def.IsAdvancedArmor() || _this.def.IsArmor())
+            Pawn crafter = _this.Position.GetEdifice(_this.Map).InteractionCell.GetFirstPawn(_this.Map);
+            float skillPerc = ((float)crafter.skills.GetSkill(SkillDefOf.Crafting).Level / 20);
+            float num = Mathf.Lerp(0.5f, 1.5f, skillPerc);
+            float healthPerc = ((float)_this.HitPoints / (float)_this.MaxHitPoints);
+            float num1 = Mathf.Lerp(0f, 0.4f, healthPerc);
+            foreach (var thingCost in costListAdj)
             {
-                Pawn crafter = _this.Position.GetEdifice(_this.Map).InteractionCell.GetFirstPawn(_this.Map);
-                float skillPerc = ((float)crafter.skills.GetSkill(SkillDefOf.Crafting).Level / 20);
-                float num = Mathf.Lerp(0.5f, 1.5f, skillPerc);
-                float healthPerc = ((float)_this.HitPoints / (float)_this.MaxHitPoints);
-                float num1 = Mathf.Lerp(0f, 0.4f, healthPerc);
-                foreach (var thingCost in costListAdj)
+                if (!thingCost.thingDef.intricate)
                 {
-                    if (!thingCost.thingDef.intricate)
+                    var PercentMaxLimit = (float)thingCost.count * num1;
+                    var mainSmeltProductCount = (PercentMaxLimit * num);
+                    if (mainSmeltProductCount > 0)
                     {
-                        var PercentMaxLimit = (float)thingCost.count * num1;
-                        var mainSmeltProductCount = (PercentMaxLimit * num);
-                        if (mainSmeltProductCount > 0)
-                        {
-                            var resultantSmeltedThing = ThingMaker.MakeThing(thingCost.thingDef, null);
-                            resultantSmeltedThing.stackCount = (int)mainSmeltProductCount;
-                            yield return resultantSmeltedThing;
-                        }
+                        var resultantSmeltedThing = ThingMaker.MakeThing(thingCost.thingDef, null);
+                        resultantSmeltedThing.stackCount = (int)mainSmeltProductCount;
+                        yield return resultantSmeltedThing;
                     }
-                }
-            }
-            else if (!costListAdj.NullOrEmpty())
-            {
-                foreach (var thingCost in costListAdj)
-                {
-                    if (!thingCost.thingDef.intricate)
-                    {
-                        var twentyFivePercentLimit = (float)thingCost.count * 0.25f;
-                        var randomUpToTwentyFivePercent = GenMath.RoundRandom(twentyFivePercentLimit);
-                        if (randomUpToTwentyFivePercent > 0)
-                        {
-                            var resultantSmeltedThing = ThingMaker.MakeThing(thingCost.thingDef, null);
-                            resultantSmeltedThing.stackCount = randomUpToTwentyFivePercent;
-                            yield return resultantSmeltedThing;
-                        }
-                    }
-                }
-            }
-            else if (!_this.def.smeltProducts.NullOrEmpty())
-            {
-                foreach (var smeltProduct in _this.def.smeltProducts)
-                {
-                    var resultantSmeltedThing = ThingMaker.MakeThing(smeltProduct.thingDef, null);
-                    resultantSmeltedThing.stackCount = smeltProduct.count;
-                    yield return resultantSmeltedThing;
                 }
             }
         }
